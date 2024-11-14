6443afd7f34c78c0d0ae2ece2dc5b77b9b4dc5d7
Vincent Voong <voongvincent@gmail.com>
2024-11-13 23:29:00 -0800

    Add npmignore

--- backend/app.py
+++ backend/app.py
@@ -21,4 +21,4 @@ def download_pdf():
     return send_file(pdf_path, as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=8080)
